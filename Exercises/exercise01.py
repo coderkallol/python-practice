7ce4a1b85119efe583851953fbc182dcdfed9f09
kallolkrnath <coderkallol.101@gmail.com>
2026-03-22 18:55:45 +0530

    Add exercise01.py with basic print statements and string manipulations

---
+++ Exercises/exercise01.py
@@ -0,0 +1,38 @@
+print("Hello, World!")
+print(2+2)
+print('2'+'2')
+print('Kallol' + 'Nath')
+print('Kallol' * 3)
+print('Kallol' + 'Nath' * 3)
+
+k = 45
+n = 40
+print(k)
+print(k + n)
+
+# For comments
+'''I
+am coding
+Know.'''
+print('''I
+am coding
+Know.''')
+
+name = 'Kallol'
+print("Hello, " + name)
+
+print("My name is Kallol.\nI am a student of CSE.")
+print("\"Hello, World!\"")
+
+print("Hello", end=" ")
+print("World")
+
+print('cats', 'dogs', 'mice', sep=', ')
+
+name = 'Kumar'
+for i in name:
+    print(i)
+
+sents = "Zophie a cat"
+newSents = sents[0:7] + 'the' + sents[8:12]
+print(newSents)
